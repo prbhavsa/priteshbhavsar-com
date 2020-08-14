b8b868b6df5d62a4eb2e90238973178c2306eed0
Robot Solution <manage.ars1@gmail.com>
2020-08-14 13:37:06 -0400

    update

--- fd/app.js
+++ fd/app.js
@@ -1,5 +1,13 @@
 
-let model, ctx, videoWidth, videoHeight, video, canvas;
+let model, ctx, videoWidth, videoHeight, video;
+let timer = false;
+let recognition = new webkitSpeechRecognition();
+let looking = false;
+
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.lang = "en-us";
+
 
 async function setupCamera() {
     video = document.getElementById('video');
@@ -27,7 +35,7 @@ const renderPrediction = async () => {
         video, returnTensors, flipHorizontal, annotateBoxes);
 
     if (predictions.length > 0) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         for (let i = 0; i < predictions.length; i++) {
             if (returnTensors) {
@@ -43,18 +51,19 @@ const renderPrediction = async () => {
             const size = [end[0] - start[0], end[1] - start[1]];
             let fDistancePer = ((start[0]-end[0])/300)*100;
             document.getElementById('fDistancePer').innerHTML = fDistancePer;
-            ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
-            ctx.fillRect(start[0], start[1], size[0], size[1]);
+            // ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+            // ctx.fillRect(start[0], start[1], size[0], size[1]);
+
 
             if (annotateBoxes) {
                 const landmarks = predictions[i].landmarks;
-                console.log("Landmarks:", predictions[i].landmarks);
+                //console.log("Landmarks:", predictions[i].landmarks);
 
-                ctx.fillStyle = "blue";
+                // ctx.fillStyle = "blue";
                 for (let j = 0; j < landmarks.length; j++) {
                     const x = landmarks[j][0];
                     const y = landmarks[j][1];
-                    ctx.fillRect(x, y, 5, 5);
+                    // ctx.fillRect(x, y, 5, 5);
                     let eyeDistance = landmarks[0][0] - landmarks[1][0];
 
                     let eyeDistancePer = (eyeDistance/150)*100;
@@ -63,12 +72,21 @@ const renderPrediction = async () => {
                         document.getElementById('vStatus').innerHTML = 'Not Looking';
                         document.getElementById('lStatus').style.color = 'red';
                         document.getElementById('lStatus').innerHTML = 'Not Listening';
+                        looking = false;
                     }
                     else{
+                        looking = true;
                         document.getElementById('vStatus').style.color = 'green';
                         document.getElementById('vStatus').innerHTML = 'Looking';
                         document.getElementById('lStatus').style.color = 'green';
                         document.getElementById('lStatus').innerHTML = 'Listening';
+                        clearTimeout(timeout);
+                        if(!timer){
+                            sayText('Hello, welcome to santa fe court',1,1,1);
+                            timer = true;
+                            console.log("TIMER:", timer);
+                        }
+                        timeout = setTimeout(()=>{ timer = false; console.log("Timer: ", timer) }, 15000);
                     }
 
                     document.getElementById('iDistancePer').innerHTML = eyeDistancePer;
@@ -86,6 +104,22 @@ const renderPrediction = async () => {
                         case 2:
                             document.getElementById('n-x').innerHTML = x;
                             document.getElementById('n-y').innerHTML = y;
+
+                            let cX = video.videoWidth/2;
+                            let cY = video.videoHeight/2;
+                            let offX = x-cX;
+                            let offY = y-cY;
+                            let rad = (Math.atan2(offY, offX) + 450 ) % 360;
+                            let deg = (rad * 180) / Math.PI;
+
+                            document.getElementById('angle').innerHTML = Math.floor(deg);
+
+                            if(deg >= 4950 || deg <=5150){
+                                continue;
+                            }
+                            else{
+                                setGaze(Math.floor(deg),1);
+                            }
                             break;
                         case 3:
                             document.getElementById('m-x').innerHTML = x;
@@ -123,72 +157,73 @@ const setupPage = async () => {
     video.width = videoWidth;
     video.height = videoHeight;
 
-    canvas = document.getElementById('output');
-    canvas.width = videoWidth;
-    canvas.height = videoHeight;
-    ctx = canvas.getContext('2d');
-    ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+    // canvas = document.getElementById('output');
+    // canvas.width = videoWidth;
+    // canvas.height = videoHeight;
+    // ctx = canvas.getContext('2d');
+    // ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
 
     model = await blazeface.load();
 
     renderPrediction();
 };
 
+let startRec = function(){
+    try{
+        recognition.stop();
+        recognition.start();
+        console.log("Recognation Started");
+    }
+    catch (e) {
+        console.log(e.message);
+    }
+};
 
-window.onload = async () => {
-    setupPage();
+let stopRec = function(){
+    try{
+        recognition.abort();
+        recognition.stop();
+        console.log("Recognation ended");
+    }
+    catch (e) {
+        console.log(e.message);
+    }
+    finally {
+        recognition.stop();
+    }
 };
 
 
+recognition.onend = function(){
+    startRec();
+    // if(!clientApp.avatar.isTalking){
+    //     startRec();
+    //     console.log("Speech recognation =  ONEND REC START");
+    // }
+    // else{
+    //     console.log("Speech recognation =  ONEND REC NOT TRIGGERED");
+    // }
+};
 
-async function main() {
-    // Load the model.
-    const model = await blazeface.load();
+recognition.onresult = function(event){
+    let transcript = event.results[event.results.length - 1][0].transcript;
+    let spResult = event.results[event.resultIndex];
 
-    // Pass in an image or video to the model. The model returns an array of
-    // bounding boxes, probabilities, and landmarks, one for each detected face.
-    let video = document.getElementById('myVid');
+    if(looking){
+        if(spResult.isFinal){
 
-    const stream = await navigator.mediaDevices.getUserMedia({
-        'audio': false,
-        'video': { facingMode: 'user' },
-    });
-    video.srcObject = stream;
-
-    const returnTensors = false; // Pass in `true` to get tensors back, rather than values.
-    const predictions = await model.estimateFaces(video, returnTensors);
-
-    if (predictions.length > 0) {
-        /*
-        `predictions` is an array of objects describing each detected face, for example:
-
-        [
-          {
-            topLeft: [232.28, 145.26],
-            bottomRight: [449.75, 308.36],
-            probability: [0.998],
-            landmarks: [
-              [295.13, 177.64], // right eye
-              [382.32, 175.56], // left eye
-              [341.18, 205.03], // nose
-              [345.12, 250.61], // mouth
-              [252.76, 211.37], // right ear
-              [431.20, 204.93] // left ear
-            ]
-          }
-        ]
-        */
-
-        for (let i = 0; i < predictions.length; i++) {
-            const start = predictions[i].topLeft;
-            const end = predictions[i].bottomRight;
-            const size = [end[0] - start[0], end[1] - start[1]];
-
-            // Render a rectangle over each detected face.
-            ctx.fillRect(start[0], start[1], size[0], size[1]);
         }
+        document.getElementById('sText').innerHTML = transcript;
     }
-}
+};
+
+
+window.onload = async () => {
+    setupPage();
+    startRec();
+};
+
+
 
 
 
